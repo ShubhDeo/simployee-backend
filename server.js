94d57de86f34d20f89e91d07e813529ac3bc18cc
shubh <subh2897@gmail.com>
2022-10-07 22:10:36 +0530

    db added

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const loginRoute = require("./routes/login");
+const connectDB = require('./config/db')
+
 
 const app = express();
 
 app.use(express.json());
 dotenv.config();
+
+
+//connect db
+connectDB();
+
 
 const PORT = process.env.PORT || 8800;
 
