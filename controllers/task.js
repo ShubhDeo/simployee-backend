7cf85c350f63644e16fbd3afade4dcf7d1efa270
Rohit Sharma <rs305541@gmail.com>
2022-10-08 23:21:06 +0530

    new task routes added

--- controllers/task.js
+++ controllers/task.js
@@ -12,14 +12,14 @@ const addTask = asyncHandler(async (req, res) => {
       res.status(400).send("Invalid Input, Cannot add task");
       return;
     } else {
-      let user=await User.findById(req.user._id).populate("tasks").exec();
-      let task=new Task({
-        user:user._id,
+      let user = await User.findById(req.user._id).populate("tasks").exec();
+      let task = new Task({
+        user: user._id,
         description,
-        timeTaken
-      })
+        timeTaken,
+      });
 
-      const createdTask=await task.save();
+      const createdTask = await task.save();
 
       user.tasks.push(createdTask);
 
@@ -29,10 +29,58 @@ const addTask = asyncHandler(async (req, res) => {
       res.json(createdTask);
     }
   } catch (error) {
-    res.status(400).send(error);
+    res.status(400).send(error.message);
   }
 });
 
-const fetchTask = asyncHandler(async (req, res) => {});
+// @desc fetch a task
+// @route GET /api/task/fetch/:user_id
+// @access Protected route
+const fetchTask = asyncHandler(async (req, res) => {
+  const { user_id } = req.params;
 
-module.exports = { addTask, fetchTask };
+  try {
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      res.status(404).send("User not found");
+      return;
+    } else {
+      res.status(200).json(user.tasks);
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+// @desc delete a task
+// @route DELETE /api/task/delete/:task_id
+// @access Protected route
+const deleteTask = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const task = await Task.findById(id);
+
+    if (!task) {
+      res.status(404).send("Task not found");
+      return;
+    } else {
+      const user = await User.findById(req.user._id);
+      await task.remove();
+      let filteredTasks = [];
+      user.tasks.map((task) => {
+        if (task._id !== id) {
+          filteredTasks.push(task);
+        }
+      });
+      user.tasks = filteredTasks;
+      await user.save();
+      res.status(200).send("Task deleted successfully");
+    }
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+module.exports = { addTask, fetchTask, deleteTask };
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const taskRouter = express.Router();
-const { addTask, fetchTask } = require("../controllers/task");
+const { addTask, fetchTask , deleteTask } = require("../controllers/task");
 const { protect } = require("../middlewares/authMiddleware");
 
 taskRouter.route("/add").post(protect, addTask);
 taskRouter.route("/fetch/:user_id").get(protect, fetchTask);
+taskRouter.route("/:id").delete(protect, deleteTask);
+
 
 module.exports = { taskRouter };
