09bdeae059f440bf84906ca264e1aec052998ce7
shubh <subh2897@gmail.com>
2022-10-08 12:36:55 +0530

    dept added

---
+++ data/dept.js
@@ -0,0 +1,7 @@
+const department = {
+    1 : 'Accounting Department', 
+    2 : 'Human Resource Department',
+    3 : 'Sales Department',
+    4 : 'Technology Department'
+}
+module.exports = department;
