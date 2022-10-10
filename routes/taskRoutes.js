9c79f180c69b6278be483907bdb4e5bf77918c5a
shubh <subh2897@gmail.com>
2022-10-10 11:36:50 +0530

    route changed

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -6,7 +6,7 @@ const { protect } = require("../middlewares/authMiddleware");
 taskRouter.route("/add").post(protect, addTask);
 taskRouter.route("/fetch/:user_id").get(protect, fetchTask);
 taskRouter.route("/:id").delete(protect, deleteTask);
-taskRouter.route(`/fetch/:user_id`).get(protect,fetchTaskByWeek)
+taskRouter.route(`/fetch/week/:user_id`).get(protect,fetchTaskByWeek)
 taskRouter.route(`/fetch/:user_id/:date`).get(protect, fetchTaskByDate);
 
 
